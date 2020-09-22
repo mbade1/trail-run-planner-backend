17cfb67889cf9467d0b7718d70cebedb43cb47a0
mbade1 <mbade314@gmail.com>
2020-09-22 17:03:22 -0500

    Controllers Updated

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,2 +1,35 @@
 class JournalsController < ApplicationController
+    def index
+        journals = Journal.all
+        render json: journals
+    end
+
+    def create
+        journal = Journal.new(journal_params)
+        if journal.save
+          render json: journal, except: [:created_at, :updated_at]
+        else
+          render json: {message: "Journal creation Failed"}
+        end
+    end
+
+    def show
+        journal = Journal.find_by(id: params[:id])
+        if journal
+          render json: journal
+        else 
+          render json: {message: "Journal not found."}
+        end 
+    end
+
+    def destroy
+        journal = Journal.find_by(id: params[:id])
+        journal.destroy
+    end
+  
+      private
+  
+      def journal_params
+          params.require(:journal).permit(:hiker_project_id, :name, :summary, :difficulty, :imgMedium, :length, :ascent, :descent, :conditionStatus, :conditionDetails, :conditionDate, :date_of_run)
+      end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+
+    def index
+      user = User.all
+      render json: user
+    end
+    
+    def create
+      user = User.new(user_params)
+      if user.save
+        render json: user, except: [:created_at, :updated_at]
+      else
+        render json: {message: "Signup Failed"}
+      end
+    end
+
+    def show
+      user = User.find_by(id: params[:id])
+      if user
+        render json: user
+      else 
+        render json: {message: "User not found."}
+      end 
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
